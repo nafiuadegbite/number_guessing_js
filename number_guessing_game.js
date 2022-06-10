4de294a0096e970740b433402e82c56804ae5f7a
nafiu adegbite <youngguru23@gmail.com>
2022-06-10 12:25:46 +0100

    made some changes

--- number_guessing_game.js
+++ number_guessing_game.js
@@ -1,40 +1,48 @@
-let quit = true;
+let start = true;
+let startMessage = "Start Game";
+const username = prompt(`Please enter your name: `);
+let welcomeUser = `Welcome ${username}\n`;
+
 const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const username = prompt(`Please enter your name: `);
-
-const numberGuessingGame = (range = 2, point = 0, stage = 1) => {
-  const guessedNum = Number(prompt(`Guess a number between 1 and ${range}: `));
-  console.log(guessedNum);
-
-  const rangeNumber = getRndInteger(1, range);
-  if (guessedNum === rangeNumber) {
-    point++;
-    range++;
-    stage++;
-    numberGuessingGame(range, point);
-    console.log(
-      `Weldone ${username}, You are unto the next stage ${stage}. Your point(s) is ${point}`
+const numberGuessingGame = (range = 2, point = 0, stage = 1, play = true) => {
+  while (play) {
+    const guessedNum = Number(
+      prompt(`Guess a number between 1 and ${range}: `)
     );
-  } else {
-    console.log(`Game over`);
-    console.log(
-      `Sorry ${username}, The correct number is ${guessedNum}. Your total point(s) is ${point}`
-    );
+    const rangeNumber = getRndInteger(1, range);
+
+    if (guessedNum === rangeNumber) {
+      point++;
+      range++;
+      stage++;
+      console.log(
+        `Weldone ${username}, You are unto the next stage ${stage}. Your point(s) is ${point}`
+      );
+    } else {
+      welcomeUser = "";
+      startMessage = "Do you want to continue";
+      play = false;
+      console.log(
+        `Game over!!!\nSorry ${username}, The correct number is ${rangeNumber}. Your total point(s) is ${point}`
+      );
+    }
   }
 };
 
 const startGame = () => {
-  while (quit) {
-    const startInstruction = Number(prompt(`Ready to play?\n1. Yes 2. Exit`));
+  while (start) {
+    const startInstruction = Number(
+      prompt(`${welcomeUser}${start}\n1. Yes\n2. No`)
+    );
     if (startInstruction === 1) {
       numberGuessingGame();
     } else if (startInstruction === 2) {
-      quit = false;
+      start = false;
     } else {
-      console.log(`Enter a valid number.`);
+      console.log(`Enter a valid number`);
     }
   }
 };
